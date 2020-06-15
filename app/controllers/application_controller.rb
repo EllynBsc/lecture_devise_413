719e8b9378c443f1b218805c1943ff50819ba714
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-06-15 10:06:35 +0200

    lecture devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # if you are on a devise controller(password, registration, session), you should before any action, run the method configure_permitted_parameters
+
+  def configure_permitted_parameters
+    # sign up user info: not asking for too much infos
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birth_date])
+
+      # update of the user information
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :age, :city ])
+  end
 end
